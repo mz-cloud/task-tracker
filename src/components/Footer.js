e7d988b0f074945b3f1b61f9d4a6683ae999eb47
vincent12112 <87328642+vincent12112@users.noreply.github.com>
2022-07-13 14:39:20 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-	return (
-		<footer>
-			<p>Copyright &copy; 2021</p>
-			<Link to="/about">About</Link>
-		</footer>
-	);
-};
+  return (
+    <footer>
+      <p>Copyright &copy; 2021</p>
+      <Link to='/about'>About</Link>
+      <p></p>
+      <Link to='/completed'>Completed</Link>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
