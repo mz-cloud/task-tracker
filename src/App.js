7a6db3674a21f0a4a65ff3956f21c53d988dc2b4
razahtet <htetminhlaing@gmail.com>
2022-07-13 23:13:21 -0700

    Added "Undo-Button" Feature
    You could undo an action (adding/deleting a task) you just did.

--- src/App.js
+++ src/App.js
@@ -5,110 +5,178 @@ import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import Undo from './components/Undo';
 
 const App = () => {
-	const [showAddTask, setShowAddTask] = useState(false);
-	const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [undos, setUndos] = useState([])
 
-	useEffect(() => {
-		const getTasks = async () => {
-			const tasksFromServer = await fetchTasks();
-			setTasks(tasksFromServer);
-		};
-		getTasks();
-	}, []);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
 
-	// Fetch Tasks
-	const fetchTasks = async () => {
-		const res = await fetch("http://localhost:5000/tasks");
-		const data = await res.json();
+    const getUndos = async () => {
+      const undosFromServer = await fetchJustHaps()
+      setUndos(undosFromServer)
+    }
 
-		return data;
-	};
+    getUndos()
+  }, []);
 
-	// Fetch Task
-	const fetchTask = async (id) => {
-		const res = await fetch(`http://localhost:5000/tasks/${id}`);
-		const data = await res.json();
+  //fetch justHaps
+  const fetchJustHaps = async () => {
+    const res = await fetch('http://localhost:5000/justDid')
+    const data = await res.json()
+    return data
+  }
+  const fetchJustHap = async (id) => {
+    const res = await fetch(`https://localhost:5000/justDid${id}`)
+    const data = await res.json()
+    return data
+  }
 
-		return data;
-	};
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
 
-	// Add Task
-	const addTask = async (task) => {
-		const res = await fetch("http://localhost:5000/tasks", {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(task),
-		});
+    return data;
+  };
 
-		const data = await res.json();
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
 
-		setTasks([...tasks, data]);
-	};
+    return data;
+  };
 
-	// Delete Task
-	const deleteTask = async (id) => {
-		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-			method: "DELETE",
-		});
-		res.status === 200 ? setTasks(tasks.filter((task) => task.id !== id)) : alert("Error Deleting This Task");
-	};
+  //Undo
+  const unDo = async () => {
+    const res = await fetch('http://localhost:5000/justDid')
+    const data = await res.json()
+    if (data.length > 0) {
+      console.log(data[data.length - 1].id)
+      let itWent = false
+      if (data[data.length - 1].stateH == "added") {
+        data[data.length - 1].addedMan = false
+        deleteTask(data[data.length - 1].id)
+        itWent = true
+      } else if (data[data.length - 1].stateH == "deleted") {
+        data[data.length - 1].addedMan = false
+        addTask(data[data.length - 1])
+        itWent = true
+      }
+      if (itWent) {
+        const res2 = await fetch(`http://localhost:5000/justDid/${data[data.length - 1].id}`, {
+          method: 'DELETE',
+        })
+      }
+    }
+  }
 
-	//Set Important
-	const setImportant = (id) => {
-		setTasks(tasks.map((task) => (task.id === id ? { ...task, important: !task.important } : task)));
-	};
+  // Add Task
+  const addTask = async (task) => {
+    task.stateH = 'added'
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
 
-	// Toggle Reminder
-	const toggleReminder = async (id) => {
-		const taskToToggle = await fetchTask(id);
-		const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const data = await res.json();
 
-		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-			method: "PUT",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(updTask),
-		});
+    if (task.addedMan) {
+      const res2 = await fetch('http://localhost:5000/justDid', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+    }
 
-		const data = await res.json();
+    setTasks([...tasks, data]);
+  };
 
-		setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)));
-	};
+  // Delete Task
+  const deleteTask = async (id) => {
+    const res3 = await fetch(`http://localhost:5000/tasks/${id}`)
+    let data3 = await res3.json()
+    data3.stateH = "deleted"
+    const res2 = await fetch('http://localhost:5000/justDid', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data3)
+    })
 
-	return (
-		<Router>
-			<div className="container">
-				<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<>
-								{showAddTask && <AddTask onAdd={addTask} />}
-								{tasks.length > 0 ? (
-									<Tasks
-										tasks={tasks}
-										onDelete={deleteTask}
-										onImportant={setImportant}
-										onToggle={toggleReminder}
-									/>
-								) : (
-									"No Tasks To Show"
-								)}
-							</>
-						}
-					/>
-					<Route path="/about" element={<About />} />
-				</Routes>
-				<Footer />
-			</div>
-		</Router>
-	);
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+    res.status === 200 ? setTasks(tasks.filter((task) => task.id !== id)) : alert("Error Deleting This Task");
+  };
+
+  //Set Important
+  const setImportant = (id) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, important: !task.important } : task)));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)));
+  };
+
+  return (
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onImportant={setImportant}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+                <Undo onUndo={unDo} />
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Undo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from './Button';
+
+const Undo = ({ onUndo }) => {
+    return (
+        <Button color='gray' text='Undo' onClick={onUndo} />
+    )
+}
+
+export default Undo
