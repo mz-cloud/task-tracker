92812560c8fa4009e124b9970c7c62326dd84c6b
ll-studio <81865868+LL-Studio@users.noreply.github.com>
2022-07-15 14:51:48 -0400

    quick fix

--- src/components/Completed.js
+++ src/components/Completed.js
@@ -1,31 +1,28 @@
-import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Completed = () => {
+	const [names, setNames] = useState([]);
 
-  const [names, setNames] = useState([])
+	useEffect(() => {
+		const getNames = async () => {
+			const namesFromServer = await fetchNames();
+			setNames(namesFromServer);
+		};
+		getNames();
+	}, []);
 
-  useEffect(() => {
-    const getNames = async () => {
-      const namesFromServer = await fetchNames()
-      setNames(namesFromServer)
-    }
-    getNames()
-  }, [])
+	const fetchNames = async () => {
+		const res = await fetch("http://localhost:5001/names");
+		const data = await res.json();
+		return data;
+	};
+	return (
+		<div>
+			{names && names.length > 0 && names.map((id, index) => <p key={index}>{id.text}</p>)}
+			<Link to="/">Go Back</Link>
+		</div>
+	);
+};
 
-  const fetchNames = async () => {
-    const res = await fetch('http://localhost:5001/names')
-    const data = await res.json()
-    return data
-  }
-  return (
-    <div>
-      {
-        names && names.length > 0 && names.map((id) => <p>{id.text}</p> )
-      }
-      <Link to='/'>Go Back</Link>
-    </div>
-  )
-}
-
-export default Completed
+export default Completed;
